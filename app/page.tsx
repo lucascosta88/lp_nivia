157a6fcd443ed67f07354b9355a271f485bdb729
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2024-09-19 01:32:47 -0300

    feat: added section4

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Section1 from "./_pages/section1";
 import Section2 from "./_pages/section2";
 import Section3 from "./_pages/section3";
+import Section4 from "./_pages/section4";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Section1 />
       <Section2 />
       <Section3 />
+      <Section4 />
     </div>
   );
 }
