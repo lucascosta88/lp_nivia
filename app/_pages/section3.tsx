4305ec2a99693968df9af6c1f7e70e13101c8956
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2024-09-19 01:15:10 -0300

    feat: added Section3 TODO:VIDEO CHANGES

---
+++ app/_pages/section3.tsx
@@ -0,0 +1,73 @@
+const Section3 = () => {
+    return ( 
+        <div className="gradient-background2 py-12">
+            <div className="flex flex-col items-center py-12">
+                <h1 className="h1">Histórias de transformação</h1>
+            </div>
+
+            <div className="flex justify-center video-container">
+                <div>
+                    <iframe 
+                        width="216" 
+                        height="385"  
+                        src="https://www.youtube.com/embed/oQ53m96jPNU" 
+                        title="YouTube video player" 
+                        frameBorder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                        allowFullScreen>
+                    </iframe>
+                </div>
+
+                <div>
+                    <iframe 
+                        width="216" 
+                        height="385"  
+                        src="https://www.youtube.com/embed/oQ53m96jPNU" 
+                        title="YouTube video player" 
+                        frameBorder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                        allowFullScreen>
+                    </iframe>
+                </div>
+
+                <div>
+                    <iframe 
+                        width="216" 
+                        height="385"  
+                        src="https://www.youtube.com/embed/oQ53m96jPNU" 
+                        title="YouTube video player" 
+                        frameBorder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                        allowFullScreen>
+                    </iframe>
+                </div>
+
+                <div>
+                    <iframe 
+                        width="216" 
+                        height="385"  
+                        src="https://www.youtube.com/embed/oQ53m96jPNU" 
+                        title="YouTube video player" 
+                        frameBorder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                        allowFullScreen>
+                    </iframe>
+                </div>
+
+                <div>
+                    <iframe 
+                        width="216" 
+                        height="385" 
+                        src="https://www.youtube.com/embed/oQ53m96jPNU" 
+                        title="YouTube video player" 
+                        frameBorder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                        allowFullScreen>
+                    </iframe>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Section3;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Section1 from "./_pages/section1";
 import Section2 from "./_pages/section2";
+import Section3 from "./_pages/section3";
 
 export default function Home() {
   return (
     <div>
       <Section1 />
       <Section2 />
+      <Section3 />
     </div>
   );
 }
